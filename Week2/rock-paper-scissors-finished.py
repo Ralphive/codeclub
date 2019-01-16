f789144d8826931e5a2d505dce3a17790001be4b
Ralphive <ralphrpo@gmail.com>
2019-01-16 18:20:04 +0000

    simpler game

--- Week2/rock-paper-scissors-finished.py
+++ Week2/rock-paper-scissors-finished.py
@@ -1,37 +1,19 @@
 #!/bin/python3
-
 from random import randint
   
 player = input('rock (r), paper (p) or scissors (s)?')
-
-if(player == 'r'):
-  print('O', end=' ')
-  
-elif(player == 'p'):
-  print('___', end=' ')
-  
-elif(player == 's'):
-  print('>8', end=' ')
-  
-else:
-  print('??')
-  
-print('vs', end=' ')
-
 chosen = randint(1,3)
 
 if(chosen == 1):
   computer = 'r'
-  print('O')
-  
+
 elif(chosen == 2):
   computer = 'p'
-  print('___')
-  
+
 else:
   computer = 's'
-  print('>8')
 
+print(player,'vs',computer)
 if(player == computer):
   print('DRAW!')
   
@@ -52,8 +34,6 @@ elif(player == 's' and computer == 'p'):
   
 elif(player == "s" and computer == 'r'):
   print('Computer wins!')
-
 else:
-  print('Huh?')
-  
-  
+  print('hun??')
+
